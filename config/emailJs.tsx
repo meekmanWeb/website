3d99b38d86100f8956f91b0c9c648ae4f5d38f63
abeebdon <abeebdon@gmail.com>
2026-03-14 19:00:34 +0100

    added emailjs

---
+++ config/emailJs.tsx
@@ -0,0 +1,3 @@
+export const serviceId = process.env.NEXT_PUBLIC_EMAIL_serviceID;
+export const templateId = process.env.NEXT_PUBLIC_EMAIL_templateID;
+export const publickKey = process.env.NEXT_PUBLIC_EMAIL_publick_key;
